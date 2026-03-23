75b9caebe83662a234a7a1bf4b23191154535ca9
Zaine <zaineirshad.07@gmail.com>
2026-03-23 07:11:00 +0000

    First Question

---
+++ quest1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class quest1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+
+        shiftByN(a, 2);
+
+        System.out.println(a.toString());
+    }
+
+    public static void shiftByN(ArrayList<Integer> a, int amount) {
+        
+    }
+}
+
