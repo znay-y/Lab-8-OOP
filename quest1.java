26f38ddec6cfc4efa2ff75c20075f3a53059a5e4
Z Laptop <zaineirshad.07@gmail.com>
2026-03-26 08:13:47 +0000

    Question 1 Solved

--- quest1.java
+++ quest1.java
@@ -15,7 +15,26 @@ public class quest1 {
     }
 
     public static void shiftByN(ArrayList<Integer> a, int amount) {
-        
+        for(int i =0; i<amount;i++){
+            pushIndex(a);
+        }
+    }
+
+    public static void pushIndex(ArrayList<Integer> xs){
+        int toPush= xs.get(0);
+        int temp;
+        System.out.println("toPush is: "+toPush);
+
+        for(int i =0;i<xs.size()-1;i++){
+            temp = xs.get(i+1);
+            System.out.println("Adding "+toPush+" where "+xs.get(i+1)+" is rn");
+            xs.set(i+1, toPush);
+            toPush = temp;
+            System.out.println("New toPush value "+toPush);
+            System.out.println("\nNew Aray "+xs.toString()+"\n");
+
+        }
+        xs.set(0, toPush);
     }
 }
 
